4bafaf0b4e5670da7a48341688d6ed9644c83d25
Yusuf karback <yusufkarback64@gmail.com>
2023-01-05 15:57:02 +0700

    ubah nama file dashboard -> ormawa, update admin

--- admin.py
+++ admin.py
@@ -7,6 +7,7 @@ from url import BASE_URL
 
 admin = Blueprint('admin', __name__)
 
+
 @admin.route('/admin')
 def show_admin():
     url = f"{BASE_URL}/ormaweb/api/v1/admin/"
@@ -17,7 +18,30 @@ def show_admin():
 
     return render_template('admin.html', data=dict['results'])
 
-@admin.route('/tambah_admin',methods = ['POST'])
+
+@admin.route('/admin/<int:id>', methods=['GET'])
+def update_form_admin(id): 
+    url = f"{BASE_URL}/ormaweb/api/v1/admin/{id}/"
+
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+
+    return render_template('update_users.html', data=dict)
+
+
+@admin.route('/admin/<int:id>', methods=['POST'])
+def update_admin(id):
+    url = f"{BASE_URL}/ormaweb/api/v1/admin/{id}/"
+
+    data_send = request.form.to_dict()
+    requests.patch(url, json=data_send)
+    print(dict)
+    print(request.form.to_dict())
+    return redirect(url_for('admin.show_admin'))
+
+
+@admin.route('/tambah_admin', methods=['POST'])
 def tambah_admin():
     url = f"{BASE_URL}/ormaweb/api/v1/admin/"
 
