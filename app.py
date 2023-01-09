529086e52966fc40a4d83c0c97e52ad6a7264ee0
Yusuf karback <yusufkarback64@gmail.com>
2023-01-09 15:48:42 +0700

    create galeri

--- app.py
+++ app.py
@@ -10,15 +10,17 @@ from ormawa import ormawa
 from admin import admin
 from auth import auth
 from kegiatan import kegiatan
+from galeri import galeri
 
 application = Flask(__name__)
 
 application.secret_key = '1768b67767bb8ac8f5b7ecdb48dbef720703387ccce086c562a4bf61000f9c31'
-
+application.config['UPLOAD_FOLDER'] = 'static/images'
 application.register_blueprint(ormawa)
 application.register_blueprint(admin)
 application.register_blueprint(auth)
 application.register_blueprint(kegiatan)
+application.register_blueprint(galeri)
 
 
 @application.route('/')
